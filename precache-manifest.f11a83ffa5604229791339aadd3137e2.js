a49ea2f554947f8bc919ccbff10704ec4992b428
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2022-03-19 08:26:34 +0000

    Deploy Zhouqchao/demo-app to github.com/Zhouqchao/demo-app.git:gh-pages

---
+++ precache-manifest.f11a83ffa5604229791339aadd3137e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a958f2cd80bda725ab4",
+    "url": "/demo-app/static/css/main.f834bd70.chunk.css"
+  },
+  {
+    "revision": "0a958f2cd80bda725ab4",
+    "url": "/demo-app/static/js/main.0a958f2c.chunk.js"
+  },
+  {
+    "revision": "13eeb2032981bf3107f9",
+    "url": "/demo-app/static/js/1.13eeb203.chunk.js"
+  },
+  {
+    "revision": "28fc3e86c9ea21c389e9",
+    "url": "/demo-app/static/js/runtime~main.28fc3e86.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/demo-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdbea2cb5cf29649c036c080920d6049",
+    "url": "/demo-app/index.html"
+  }
+];
